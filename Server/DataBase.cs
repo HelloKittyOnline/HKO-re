247b2c18198edc5439d37e2ce168db3a2069dd4f
Redcrafter <redcrafter@hotmail.de>
2022-01-25 19:13:04 +0100

    Implemented password hashing

--- Server/DataBase.cs
+++ Server/DataBase.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Text.Json;
 
 namespace Server {
     class Account {
         public string Username { get; set; }
-        public string Password { get; set; }
+
+        public byte[] Salt { get; set; }
+        public byte[] Password { get; set; }
+
         public PlayerData PlayerData { get; set; }
     }
 
@@ -27,17 +32,38 @@ namespace Server {
 
         public Account GetPlayer(string username, string password) {
             if(Accounts.TryGetValue(username, out var account)) {
-                return account.Password == password ? account : null;
+                return VerifyPassword(password, account) ? account : null;
             }
 
             Console.WriteLine($"Created account {username}");
+
+            var salt = GenerateSalt();
+
             // create new account
             account = new Account {
                 Username = username,
-                Password = password
+                Salt = salt,
+                Password = HashPassword(salt, password)
             };
             Accounts[username] = account;
             return account;
+        }
+
+        private static byte[] GenerateSalt() {
+            byte[] salt = new byte[128 / 8];
+            var rngCsp = new RNGCryptoServiceProvider();
+            rngCsp.GetNonZeroBytes(salt);
+            return salt;
+        }
+
+        private static byte[] HashPassword(byte[] salt, string password) {
+            var rfc = new Rfc2898DeriveBytes(password, salt, 10000);
+            return rfc.GetBytes(256 / 8);
+        }
+
+        private static bool VerifyPassword(string password, Account account) {
+            var hash = HashPassword(account.Salt, password);
+            return hash.SequenceEqual(account.Password);
         }
     }
 }
