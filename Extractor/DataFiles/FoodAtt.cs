4606787fb7fc947c0b664a25aa3eea1c9a7bdee8
Redcrafter <redcrafter@hotmail.de>
2026-03-07 01:30:49 +0100

    fixed FoodAtt

--- Extractor/DataFiles/FoodAtt.cs
+++ Extractor/DataFiles/FoodAtt.cs
@@ -6,12 +6,12 @@ public struct FoodAtt {
 
     [SeanField(1)] public int EnergyRestored { get; set; }
     [SeanField(2)] public int ActionPointsRestored { get; set; }
-    [SeanField(3)] public int Duration { get; set; }
+    [SeanField(3)] public int BuffId { get; set; }
     [SeanField(4)] public int Unknown4 { get; set; }
     [SeanField(5)] public int Unknown5 { get; set; }
     [SeanField(6)] public int Unknown6 => 0;
-    [SeanField(7)] public string FileName { get; set; }
-    [SeanField(8)] public int Unknown8 { get; set; }
+    [SeanField(7)] public string TransformationTexture { get; set; }
+    [SeanField(8)] public int TransformationDuration { get; set; }
     [SeanField(9)] public int Unknown9 { get; set; }
     [SeanField(10)] public int Unknown10 => 0;
 }
